5ef71433a1ef91874c0e998b608168b92119ffc2
paul <yummta@gmail.com>
2019-05-29 02:09:13 -0500

    Update map with location

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Mymap() {
+  const [position, setPosition] = React.useState([0, 0]);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const getPosition = new Promise((fulfillet, reject) => {
+    navigator.geolocation.getCurrentPosition(position => {
+      fulfillet(position);
+    });
+  });
+
+  React.useEffect(() => {
+    getPosition.then(position => {
+      setPosition([position.coords.latitude, position.coords.longitude]);
+    });
+  }, [position]);
+
+  function handleChange() {
+    setPosition([32.5355, 44.4275]);
+  }
+
+  return (
+    <>
+      <Map center={position} zoom={15}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup.
+            <br />
+            Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+      <select onChange={handleChange}>
+        <option value="a">value A</option>
+        <option value="b">value B</option>
+        <option value="c">value C</option>
+      </select>
+    </>
+  );
+}
+
+const $root = document.getElementById("root");
+render(<Mymap />, $root);
