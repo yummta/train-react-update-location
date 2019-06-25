9b173a52d9dc179913b32af3f05fdc77d72f039d
paul <yummta@gmail.com>
2019-06-25 16:18:45 -0500

    Get data for the map from an API
    - Get data and fill the select input
    - Update map with data obtained

--- src/index.js
+++ src/index.js
@@ -1,29 +1,62 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 import React from "react";
 import { render } from "react-dom";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 function Mymap() {
   const [position, setPosition] = React.useState([0, 0]);
+  const [locations, setLocations] = React.useState([]);
+  const [current, setCurrent] = React.useState(1);
 
-  const getPosition = new Promise((fulfillet, reject) => {
+  const getPosition = new Promise(fulfillet => {
     navigator.geolocation.getCurrentPosition(position => {
       fulfillet(position);
     });
   });
 
+  const coords = fetch(
+    "https://my-json-server.typicode.com/yummta/my-json-server/locations"
+  );
+
   React.useEffect(() => {
     getPosition.then(position => {
       setPosition([position.coords.latitude, position.coords.longitude]);
     });
-  }, [position]);
+  }, [current]);
 
-  function handleChange() {
-    setPosition([32.5355, 44.4275]);
+  React.useEffect(() => {
+    coords
+      .then(response => response.json())
+      .then(locations => setLocations(locations));
+  }, []);
+
+  function handleChange(e) {
+    let idLocal = parseInt(e.target.value);
+    if (!idLocal) {
+      setCurrent(current + 1);
+      return;
+    }
+    let locationSelected = locations.find(local => local.id === idLocal);
+    setPosition([locationSelected.lat, locationSelected.lon]);
   }
 
   return (
     <>
-      <Map center={position} zoom={15}>
+      <header css={{ display: "flex", justifyContent: "center", padding: 16 }}>
+        <h1 css={{ fontSize: 16, marginRight: 24 }}>
+          Seven Wonders of the Ancient World
+        </h1>
+        <select onChange={handleChange}>
+          <option defaultValue={0}>Your current location</option>
+          {locations.map(location => (
+            <option key={location.id} value={location.id}>
+              {location.name}
+            </option>
+          ))}
+        </select>
+      </header>
+      <Map center={position} zoom={13}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -36,11 +69,6 @@ function Mymap() {
           </Popup>
         </Marker>
       </Map>
-      <select onChange={handleChange}>
-        <option value="a">value A</option>
-        <option value="b">value B</option>
-        <option value="c">value C</option>
-      </select>
     </>
   );
 }
